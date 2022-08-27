5286ac15e2c432469e2939c6818a8476df7279b6
ivankrzak <krzak.ivan@gmail.com>
2022-08-27 13:04:13 +0200

    Error handling

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 /* eslint-disable @typescript-eslint/naming-convention */
+import { ApolloError } from 'apollo-errors'
 import { ApolloServer } from 'apollo-server-micro'
 import Cors from 'micro-cors'
 import { createContext } from '../../api/graphql/context'
@@ -10,6 +11,12 @@ const cors = Cors()
 const apolloServer = new ApolloServer({
   schema: Schema,
   context: createContext,
+  formatError: (error) => {
+    if (!(error.originalError instanceof ApolloError)) {
+      return new Error('Something went wrong.')
+    }
+    return error
+  },
 })
 
 const startServer = apolloServer.start()
